d0138913dba48f80e99bdb46c3405da8baecd8eb
admin <dominique.bertin@geonosis.fr>
2022-02-05 12:43:17 +0100

    Comment

--- src/main/java/fr/imt/ales/cepi/demo/paint/PaintServlet.java
+++ src/main/java/fr/imt/ales/cepi/demo/paint/PaintServlet.java
@@ -1,6 +1,5 @@
 package fr.imt.ales.cepi.demo.paint;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,18 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * The servlet for the paint page. Handle only GET method. Dispatch to paint.jsp
+ */
 @WebServlet("/paint")
 public class PaintServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        RequestDispatcher rd = request.getRequestDispatcher("jsp/paint.jsp");
-        rd.forward(request, response);
-    }
-
-    public void destroy() {
+        request.getRequestDispatcher("jsp/paint.jsp").forward(request, response);
     }
 }
